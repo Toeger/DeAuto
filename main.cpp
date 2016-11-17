98ce7fba09ae3b2e486c87e7642744aa59b91652
toeger <toeger@anonymouse.me>
2016-11-18 00:00:02 +0100

    Able to print out declarations and their file and line number, but colors don't work.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,13 @@
 #include "main.h"
 
+#include <chrono>
 #include <iostream>
 
-// Declares clang::SyntaxOnlyAction.
-//#include "clang/Frontend/FrontendActions.h"
-#include "clang/Tooling/CommonOptionsParser.h"
-#include "clang/Tooling/Tooling.h"
-// Declares llvm::cl::extrahelp.
-#include "clang/ASTMatchers/ASTMatchFinder.h"
-//#include "clang/ASTMatchers/ASTMatchers.h"
-#include "llvm/Support/CommandLine.h"
-
-using namespace clang;
-using namespace clang::ast_matchers;
-using namespace clang::tooling;
-using namespace llvm;
+#include <clang/ASTMatchers/ASTMatchFinder.h>
+#include <clang/Tooling/CommonOptionsParser.h>
+#include <clang/Tooling/Tooling.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/raw_os_ostream.h>
 
 // Apply a custom category to all command-line options so that they are the
 // only ones displayed.
@@ -23,17 +16,16 @@ static llvm::cl::OptionCategory MyToolCategory("my-tool options");
 // CommonOptionsParser declares HelpMessage with a description of the common
 // command-line options related to the compilation database and input files.
 // It's nice to have this help message in all tools.
-static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+static llvm::cl::extrahelp CommonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
 
 // A help message for this specific tool can be added afterwards.
-static cl::extrahelp MoreHelp("\nMore help text...");
+static llvm::cl::extrahelp MoreHelp("\nMore help text...");
 
-class DeclarationNamePrinter : public MatchFinder::MatchCallback {
+class DeclarationNamePrinter : public clang::ast_matchers::MatchFinder::MatchCallback {
 	public:
-	void run(const MatchFinder::MatchResult &result) override {
-		if (const DeclStmt *ds = result.Nodes.getNodeAs<clang::DeclStmt>("declaration")) {
-			std::string output;
-			llvm::raw_string_ostream out(output);
+	void run(const clang::ast_matchers::MatchFinder::MatchResult &result) override {
+		if (const clang::DeclStmt *ds = result.Nodes.getNodeAs<clang::DeclStmt>("declaration")) {
+			llvm::raw_os_ostream out{std::cout};
 			for (auto &decl : ds->decls()) {
 				{
 					std::string filename = result.SourceManager->getFilename(decl->getLocation());
@@ -41,24 +33,34 @@ class DeclarationNamePrinter : public MatchFinder::MatchCallback {
 						continue;
 					}
 				}
-				decl->print(out, 0, false);
+				decl->print(out, 0, true);
+				out.changeColor(out.BLUE);
+				out << " in " << result.SourceManager->getFilename(decl->getLocation());
+				out.resetColor();
+				out << ":";
+				out.changeColor(out.GREEN);
+				out << result.SourceManager->getLineNumber(result.SourceManager->getFileID(decl->getLocation()),
+														   result.SourceManager->getFileOffset(decl->getLocation()));
 				out << '\n';
+				out.resetColor();
 			}
-			out.flush();
-			std::cout << output;
 		}
 	}
 };
 
 int main(int argc, const char **argv) {
-	CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
-	ClangTool Tool(OptionsParser.getCompilations(), OptionsParser.getSourcePathList());
+	auto start = std::chrono::high_resolution_clock::now();
+	std::ios::sync_with_stdio(false);
+	clang::tooling::CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
+	clang::tooling::ClangTool Tool(OptionsParser.getCompilations(), OptionsParser.getSourcePathList());
 
 	DeclarationNamePrinter declprinter;
-	StatementMatcher declmatcher = declStmt().bind("declaration");
+	clang::ast_matchers::StatementMatcher declmatcher = clang::ast_matchers::declStmt().bind("declaration");
 
-	MatchFinder Finder;
+	clang::ast_matchers::MatchFinder Finder;
 	Finder.addMatcher(declmatcher, &declprinter);
 
-	return Tool.run(newFrontendActionFactory(&Finder).get());
+	auto retval = Tool.run(clang::tooling::newFrontendActionFactory(&Finder).get());
+	std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() / 1000. << "s\n";
+	return retval;
 }
